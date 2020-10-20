2266f3069c7895cbcec33bcd06242d372197237d
Himanshu Yadav <ydv1608@gmail.com>
2020-10-20 18:24:09 +0530

    Official work on project starts from here

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-  echo 'Adding HoundCI for better code quality';  
-?>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  Starting project here
+  <?php
+        echo "Hello World";
+  ?>
+</body>
+</html>
