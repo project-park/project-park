cb674aaad5eda213d04be6e7bbf39b39b168e2a4
Himanshu Yadav <ydv1608@gmail.com>
2020-11-16 03:56:40 +0530

    Added hotshot functionality

--- php/profile.php
+++ php/profile.php
@@ -55,31 +55,32 @@
             for($i=0;$i<count($projects);$i++) { ?>
 
             <div class="col s12 m6 l4">
-                <div class="card pink lighten-4 z-depth-5  ">
-                    <div class="card-image">
+                <div class="card pink lighten-4 z-depth-5 hotshot-parent">
+                    <div class="card-image hotshot-child1">
                         <img src=<?php echo "/img/".$images[$random_keys[$i]]; ?> alt="ballu ji ki image">
-                        <a href="#" class="btn-floating red pulse halfway-fab">
-                            <i class="material-icons ">whatshot</i>
+                        <a class="btn-floating halfway-fab red pulse hotshot-child-icon">
+                            <i class="material-icons" <?php echo "id=\"$i\""; ?>>whatshot</i>
                         </a>
-                        <a href="#" style="" class="btn-floating green halfway-fab left">
+                        <a href="#" class="btn-floating halfway-fab green left hotshot-child-num">
                             <?php echo $projects[$i]['project_stars'];?>
                         </a>
                     </div>
-                    <div class="card-content">
+                    <div class="card-content hotshot-child2">
                         <span class="card-title"><b><?php echo $projects[$i]['project_name']?></b></span>
                         <p>One-liner description is what we all crave for:)</p>
                     </div>
                     <div class="card-action">
+                        <?php if($profile !== $_SESSION['user_name']) { ?>
                         <a class="purple-text" href="#">
                             <strong>
                                 Trick Project
                             </strong>
                         </a>
+                        <?php } ?>
                         <?php
                             $link = urlencode($profile."+".$projects[$i]['project_name']);
                         ?>
-                        <a class="purple-text" target="_blank" 
-                        <?php echo "href=\"/php/recursion.php?src=$link\""; ?>>
+                        <a class="purple-text" target="_blank" <?php echo "href=\"/php/recursion.php?src=$link\""; ?>>
                             <strong>
                                 Show Project
                             </strong>
@@ -94,6 +95,35 @@
     <?php
         require "$_SERVER[DOCUMENT_ROOT]/php/footer.php";
     ?>
+    <script>
+    const iconList = document.querySelectorAll("div.hotshot-parent > div.hotshot-child1 > a.hotshot-child-icon");
+    const numList = document.querySelectorAll("div.hotshot-parent > div.hotshot-child1 > a.hotshot-child-num");
+    const projectList = document.querySelectorAll("div.hotshot-parent > div.hotshot-child2 > span");
+    console.log(projectList);
+    for (i = 0; i < iconList.length; i++) {
+        iconList[i].addEventListener("click", () => {
+            try {
+                reqElement = numList[parseInt(event.target.id)];
+                reqProject = projectList[parseInt(event.target.id)];
+                projectName = reqProject.firstChild.innerHTML;
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', "./addHotShot.php?profile=" + <?php echo "\"$profile\""?> + "&project=" +
+                    projectName, true);
+                xhr.onload = function() {
+                    if (this.status === 200) {
+                        console.log(this.responseXML.firstChild.firstChild.innerHTML);
+                    } else {
+                        console.log("Some error occured");
+                    }
+                }
+                xhr.send();
+                reqElement.innerHTML = parseInt(reqElement.innerHTML) + 1;
+            } catch (err) {
+                console.log("Exception Occured: " + err);
+            }
+        });
+    }
+    </script>
 </body>
 
 </html>
